cf2f65dd6e3b05752552be5a553e33bc4205209a
Jordan Demmouche <jordand75020@hotmail.fr>
2017-05-04 17:50:41 +0200

    validation on form

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,3 +1,22 @@
-class InformationsController < ApplicationController
+class InfosController < ApplicationController
+skip_before_action :authenticate_user!
+  def create
+    @info = Info.new(info_params)
+    if @info.save
+      redirect_to root_path
+    else
+      redirect_to request.referer
+    end
+
+  end
+private
+
+  def set_info
+      @info = info.find(params[:id])
+  end
+
+  def info_params
+    params.require(:info).permit(:first_name, :last_name, :email, :phone, :company_name, :description)
+  end
 
 end
--- app/models/info.rb
+++ app/models/info.rb
@@ -1,2 +1,8 @@
 class Info < ApplicationRecord
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :phone, presence: true, format: { with: /([0]{1})([6-7]{1})[0-9]{8}/ }
+  validates :company_name, presence: true
+  validates :description, presence: true
+  validates :email, presence: true, format: { with: /[a-z0-9]+[_a-z0-9\.-]*[a-z0-9]+@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})/ }
 end
